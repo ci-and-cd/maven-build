3550305f4b7d94a5920213c709e8ee9d24084b59
haolun <haolun@example.com>
2018-07-11 12:08:36 +0800

    update ci script

--- src/main/ci-script/lib_ci.sh
+++ src/main/ci-script/lib_ci.sh
@@ -647,7 +647,7 @@ function run_mvn() {
     local filter_script_file=$(filter_script "$(ci_opt_cache_directory)/filter")
     echo "mvn ${CI_OPT_MAVEN_SETTINGS} ${altered} | ${filter_script_file}"
     if [ "${CI_OPT_DRYRUN}" != "true" ]; then
-        bash -c "set -e -o pipefail; mvn ${CI_OPT_MAVEN_SETTINGS} -U -e ${altered} | ${filter_script_file}"
+        bash -c "set -e -o pipefail; mvn ${CI_OPT_MAVEN_SETTINGS} ${altered} | ${filter_script_file}"
     fi
 }
 
