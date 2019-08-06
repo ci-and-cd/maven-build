edbb5e0ad7effc9c097a0759e336c7b998f6e707
haolun zhang <chshawkn@users.noreply.github.com>
2019-08-06 15:48:54 +0800

    Maven Central first. upgrade topinfra-maven extensions

--- circ.sh
+++ circ.sh
@@ -46,8 +46,8 @@ if [[ -z "${CI_OPT_MAVEN_BUILD_OPTS_REPO_REF}" ]]; then
   if [[ "${GITLAB_CI}" == "true" ]]; then export CI_OPT_MAVEN_BUILD_OPTS_REPO_REF="${CI_COMMIT_REF_NAME:-develop}"; fi;
   if [[ "${TRAVIS_ENABLED}" == "true" ]]; then export CI_OPT_MAVEN_BUILD_OPTS_REPO_REF="${TRAVIS_BRANCH:-develop}"; fi;
 fi;
-#if [[ "${APPVEYOR_ENABLED}" == "true" ]]; then export SETTINGS_GLOBAL_XML="${SETTINGS_GLOBAL_XML:-/home/appveyor/.m2/wrapper/dists/topinfra-maven-dist-1.1.1/44uavdht26vl6k299u2ig39i9e/apache-maven-3.6.1/conf/settings.xml}"; fi;
-##if [[ "${GITLAB_CI}" == "true" ]]; then export SETTINGS_GLOBAL_XML="${SETTINGS_GLOBAL_XML:-/var/lib/gitlab-runner/.m2/wrapper/dists/topinfra-maven-dist-1.1.1/44uavdht26vl6k299u2ig39i9e/apache-maven-3.6.1/conf/settings.xml}"; fi;
+#if [[ "${APPVEYOR_ENABLED}" == "true" ]]; then export SETTINGS_GLOBAL_XML="${SETTINGS_GLOBAL_XML:-/home/appveyor/.m2/wrapper/dists/topinfra-maven-dist-1.3.0/4r2l54u59h15rotm6473ubn2rc/apache-maven-3.6.1/conf/settings.xml}"; fi;
+##if [[ "${GITLAB_CI}" == "true" ]]; then export SETTINGS_GLOBAL_XML="${SETTINGS_GLOBAL_XML:-/var/lib/gitlab-runner/.m2/wrapper/dists/topinfra-maven-dist-1.3.0/4r2l54u59h15rotm6473ubn2rc/apache-maven-3.6.1/conf/settings.xml}"; fi;
 #if [[ "${GITLAB_CI}" == "true" ]]; then export SETTINGS_GLOBAL_XML="${SETTINGS_GLOBAL_XML:-/tmp/settings-global-${CI_COMMIT_SHA}.xml}"; fi;
-#if [[ "${TRAVIS_ENABLED}" == "true" ]]; then export SETTINGS_GLOBAL_XML="${SETTINGS_GLOBAL_XML:-/home/travis/.m2/wrapper/dists/topinfra-maven-dist-1.1.1/44uavdht26vl6k299u2ig39i9e/apache-maven-3.6.1/conf/settings.xml}"; fi;
+#if [[ "${TRAVIS_ENABLED}" == "true" ]]; then export SETTINGS_GLOBAL_XML="${SETTINGS_GLOBAL_XML:-/home/travis/.m2/wrapper/dists/topinfra-maven-dist-1.3.0/4r2l54u59h15rotm6473ubn2rc/apache-maven-3.6.1/conf/settings.xml}"; fi;
 #export SETTINGS_GLOBAL_XML_URL="${CI_OPT_MAVEN_BUILD_OPTS_REPO}/raw/${CI_OPT_MAVEN_BUILD_OPTS_REPO_REF}/src/main/maven/settings-global.xml";
